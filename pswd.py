880b071e901eae8f9a06145301ce54ea8f4e660c
Joseph Careless <joe@care>
2024-09-13 20:05:09 +0200

    Adding new passowrd

--- pswd.py
+++ pswd.py
@@ -6,6 +6,24 @@ def load_key():
     file.close()
     return key
 
-
 key = load_key()
 fer = Fernet(key)
+
+def add():
+    name = input('Account Name: ')
+    pwd = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+
+
+while True:
+    command = input(
+        "Would you like to add a new password press q to quit?")
+    if command == "q":
+        break
+    elif command == "add":
+        add()
+    else:
+        print("Invalid command.")
+        continue
