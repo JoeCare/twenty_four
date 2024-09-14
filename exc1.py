eff5ec7998ff44c557aafebe2ee0248e68618316
Joseph Careless <joe@care>
2024-09-14 06:42:39 +0200

    Algorithyms factorial with comments

--- exc1.py
+++ exc1.py
@@ -17,6 +17,37 @@ print(fibonacci_recursive(11))
 def nwd_recursive(a, b):
     return nwd_recursive(b, a % b) if b else a
 
-# print(list(map(NWD, numbers)))
-print("nwd", nwd_recursive(14, 12))
+def nwd_iter(a, b):
+    while b:
+        a, b = b, a % b
+    return a
 
+
+# print(list(map(NWD, numbers)))
+print("nwd", nwd_recursive(15, 56)) 
+
+
+def factorial(n): return n * factorial(n-1) if n > 1 else 1
+
+4 * 3 * 2 
+# 4 * 3 = 12
+# 12 * 2 = 24
+
+# 4 * 3 = 12
+# 12 * 2 = 24
+print("rec", factorial(5))
+
+def factorial_iter(n):
+    f = 1
+    for i in range(2, n+1):
+        # print(f, i)
+        f = f*i 
+        # print("p", f, i)
+    return f
+# 1 * 2 = 2
+# 2 * 3 = 6
+# 6 * 4 = 24
+# 24 * 5 = 120
+# print(24 * 6)
+# print(factorial(5))
+print("iter", factorial_iter(5))
